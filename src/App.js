c197e15e608277949ee1f4f9c352ec69fe15becb
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-04-14 20:31:57 -0500

    third commit, exp w/hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
 import Input from './Components/Input'
 import Table from './Components/Table'
 
-
 function App() {
+  const [page, setPage] = useState(1);
+
+useEffect(() => {
+  fetch (
+    'https://swapi.dev/api/people/'
+  )
+}, [])
+
   return (
     <div>
       <header>
