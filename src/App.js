d57c300a089bdbac52727482eaec7a3e6853e0e3
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-04-17 14:03:47 -0500

    adding homeworld

--- src/App.js
+++ src/App.js
@@ -2,36 +2,22 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Input from './Components/Input'
 import Table from './Components/Table'
 
 const peopleURL = 'https://swapi.dev/api/people/';
-const planetURL = 'https://swapi.dev/api/planets/';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
 
-useEffect(() => {
-  fetch(peopleURL)
-  .then(results => results.json())
-  .then(characters => {
-  setCharacters(characters.results)
-  console.log('characters:' , characters.results)
+  useEffect(() => {
+    fetch(peopleURL)
+      .then((results) => results.json())
+      .then((characters) => {
+        setCharacters(characters.results)
+   // console.log('characters:', characters.results)
   })
 }, [characters])
-
-const getPlanetData = async (characters) =>{
-  characters.homeworld = await planetURL(characters.homeworld)
-}
-
-const planetData = () => {
-  fetch(planetURL)
-  .then(results => results.json)
-  .then(planet => {
-    planet(characters.homeworld)
-  })
-}
 
   return (
     <div>
